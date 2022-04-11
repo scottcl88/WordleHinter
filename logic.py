7c5809d7085afe990a97e783fe8c31668dedf217
Scott Lewis <scottcl@outlook.com>
2022-04-11 13:53:53 -0400

    Completing logic

--- logic.py
+++ logic.py
@@ -1,6 +1,7 @@
 import json
-from tkinter import Tk, Label, Button
 import http.client
+from posixpath import split
+import random
 import re
 import datetime
 
@@ -8,6 +9,7 @@ class Logic:
     word_list = []
     possible_words = []
     synonyms = []
+
     def __init__(self):
         self.master = self
 
@@ -16,36 +18,47 @@ class Logic:
         headers = {
             'X-RapidAPI-Host': "wordsapiv1.p.rapidapi.com",
             'X-RapidAPI-Key': "0fdf58d79cmshe4a721e7dc6a96fp18ce0cjsna339775e39bf"
-            }
+        }
         conn.request("GET", "/words/"+word+"/typeOf", headers=headers)
         res = conn.getresponse()
         data = res.read()
-        # print(data.decode("utf-8"))
-        json_object = json.loads(data)    
+        # print(data.decode("utf-8"))#prints results
+        json_object = json.loads(data)
         all_syn = json_object["typeOf"]
-        return all_syn[:2]
-    
+        if len(all_syn) > 0:
+            return random.sample(all_syn, 2 if len(all_syn) >= 2 else 1)
+        else:
+            return all_syn
+
     def load(self):
         print("Loading...")
         self.word_list = open("words.txt", 'r').read().split("\n")
         print("Loaded.")
-        # print(self.word_list)
 
     def get_exact_word(self):
         today = datetime.date.today()
-        firstday = datetime.date(2021, 6, 19)
+        firstday = datetime.date(2021, 6, 19)  # first day of wordle
         diff = today - firstday
-        print(diff.days)
-        exactword = self.word_list[diff.days]
-        print("Exact word: "+exactword)
+        return self.word_list[diff.days]
 
-    def check(self, guess):
-        # print("Checking guess: "+guess)
+    def check(self, guess: str, known_correct_letters: str, known_wrong_letters: str):
         r = re.compile(guess)
-        self.possible_words = list(filter(r.match, self.word_list))
-        # print(self.possible_words)
-        # print("Checking done")
-        for w in self.possible_words:    
-            # print("Getting Synonyms for: "+w)
-            self.synonyms += self.get_synonyms(w)
-        
+        cur_possible_words = list(filter(r.match, self.word_list))
+        new_possible_words: list[str] = []
+        for w in cur_possible_words:
+            if re.search("(["+known_correct_letters+"])+", w) and not re.search("(["+known_wrong_letters+"])+", w):
+                new_possible_words.append(w)
+        self.possible_words = new_possible_words
+        print("Possible words remaining: ", len(self.possible_words))
+        if len(self.possible_words) <= 5:
+            unknown_syn: int = 0
+            for w in self.possible_words:
+                result = self.get_synonyms(w)
+                if len(result) > 0:
+                    self.synonyms += result
+                else:
+                    unknown_syn += 1
+            print("Words without synonyms found: ", unknown_syn)
+            print("Possible synonyms are: ", self.synonyms)
+        else:
+            print("Need to narrow it down further to get help")
--- main.py
+++ main.py
@@ -1,33 +1,67 @@
 from typing import final
 from logic import Logic
 
+
 def is_empty(string):
     return not string.strip()
 
+
 class User:
     known_word = ""
-    known_letters = ""
-    def __init__(self):
-        self.master = self
-    def get_letters(self):
-        print("Enter the 5 letters you know with the correct place, otherwise add '*' in that place:")
-        self.known_word = input()       
-        self.known_word = self.known_word.replace("*",".*")
-        print("Enter any letters that you know but are in the wrong place:")
-        self.known_letters = input() 
-        print(self.known_word)      
-        print(self.known_letters)
+    known_correct_letters = ""
+    known_wrong_letters = ""
+    logic: Logic
 
+    def __init__(self, logic):
+        self.master = self
+        self.logic = logic
+
+    def get_letters(self):
+        count: int = 0
+        while count != 5:
+            print("Enter the 5 letters you know with the correct place, otherwise add '*' in that place:")
+            self.known_word = input().lower()        
+            self.known_word = self.known_word.replace("*", ".")
+            count = len(self.known_word)
+            if count != 5:
+                print("You must enter 5 characters")
+
+    def get_known_letters(self):
+        print("Enter any other letters that you know ARE in the word but in the wrong place:")
+        self.known_correct_letters = input().lower()
+        print("Enter any letters that you know are NOT in the word:")
+        self.known_wrong_letters = input().lower()
+
+    def get_more_help(self):
+        print("Need more help? Show synonyms of correct word only?")
+        more_help = input("1 for yes, anything else for no: ")
+        if more_help == "1":
+            exact_word = self.logic.get_exact_word()
+            self.logic.synonyms = []
+            new_synonyms = self.logic.get_synonyms(exact_word)
+            print("\n\nPossible synonyms are: ", new_synonyms)
+
+    def get_exact_word(self):
+        print("Give up? Want to know the answer?")
+        find_exact_word = input("1 for yes, anything else for no: ")
+        if find_exact_word == "1":
+            exact_word = self.logic.get_exact_word()
+            print("\nAnswer is: ", exact_word)
 
 def main():
     logic = Logic()
     logic.load()
-    user = User()
+    print("\n")
+    user = User(logic)
     user.get_letters()
-    print("Checking...")
-    logic.check(user.known_word)
-    print("Possible words remaining: ",len(logic.possible_words))
-    print("Possible Synonyms are: ",logic.synonyms)
+    user.get_known_letters()
+    print("\nChecking...\n\n")
+    logic.check(user.known_word, user.known_correct_letters, user.known_wrong_letters)
+    print("\n")
+    user.get_more_help()
+    print("\n")
+    user.get_exact_word()
+
 
 if __name__ == "__main__":
     main()
